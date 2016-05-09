224d3f3ac6cf356499044abd0c07e5c3658262c7
hgcummings <harry@xlr3.com>
2016-05-09 21:32:12 +0100

    Chapter 09: Implementing user rankings with Redis

--- src/services/users.js
+++ src/services/users.js
@@ -6,5 +6,34 @@ module.exports = {
     getUsername: userId =>
         redisClient.getAsync(`user:${userId}:name`),
     setUsername: (userId, name) =>
-        redisClient.setAsync(`user:${userId}:name`, name)
+        redisClient.setAsync(`user:${userId}:name`, name),
+    recordWin: userId =>
+        redisClient.zincrbyAsync('user:wins', 1, userId),
+    getTopPlayers: () =>
+        redisClient.zrevrangeAsync('user:wins', 0, 2, 'withscores')
+        .then(interleaved => {
+            if (interleaved.length === 0) {
+                return [];
+            }
+            let userIds = interleaved
+                .filter((user, index) => index % 2 === 0)
+                .map((userId) => `user:${userId}:name`);
+            return redisClient.mgetAsync(userIds)
+                .then(names => names.map((username, index) => ({
+                    name: username,
+                    userId: interleaved[index * 2],
+                    wins: parseInt(interleaved[index * 2 + 1], 10)
+                })));
+        }),
+    getRanking: userId => {
+        return Promise.all([
+            redisClient.zrevrankAsync('user:wins', userId),
+            redisClient.zscoreAsync('user:wins', userId)
+        ]).then(out => {
+            if (out[0] === null) {
+                return { rank: null, wins: 0 };
+            }
+            return { rank: out[0] + 1, wins: parseInt(out[1], 10) };
+        });
+    }
 };
--- test/services/users.js
+++ test/services/users.js
@@ -22,4 +22,54 @@ describe('User service', function() {
                 .then(() => done(), done);
         });
     });
+    
+    describe('rankings', function() {
+        let users = [
+            { userId: 'user1', name: 'One' },
+            { userId: 'user2', name: 'Two' },
+            { userId: 'user3', name: 'Three' },
+            { userId: 'user4', name: 'Four' },
+            { userId: 'user5', name: 'Five' }
+        ];
+            
+        before(function(done) {
+            let setup = [];
+            for (let i = 0; i < users.length; ++i) {
+                setup.push(service.setUsername(users[i].userId, users[i].name));
+                for (let j = 0; j <= i; ++j) {
+                    setup.push(service.recordWin(users[i].userId));
+                }
+            }
+            
+            Promise.all(setup).then(() => done(), done);
+        });
+        
+        it('should provide the top scoring players along with number of wins', function(done) {
+            service.getTopPlayers()
+                .then(topPlayers => {
+                    expect(topPlayers[0]).to.eql({ userId: 'user5', name: 'Five', wins: 5});
+                    expect(topPlayers[1]).to.eql({ userId: 'user4', name: 'Four', wins: 4});
+                    expect(topPlayers[2]).to.eql({ userId: 'user3', name: 'Three', wins: 3});
+                })
+                .then(() => done(), done);
+        });
+        
+        it('should provide the rank and score for a player with wins', function(done) {
+            service.getRanking('user4')
+                .then(result => {
+                    expect(result.rank).to.equal(2);
+                    expect(result.wins).to.equal(4);
+                })
+                .then(() => done(), done);
+        });
+        
+        it('should provide the rank and score for a player with no wins', function(done) {
+            service.getRanking('other')
+                .then(result => {
+                    expect(result.rank).to.equal(null);
+                    expect(result.wins).to.equal(0);
+                })
+                .then(() => done(), done);
+        });
+    });
 });
